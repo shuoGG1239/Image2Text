11f8415b81fe406def6dc78d1a6b3d366ff54160
shuoGG <328893769@qq.com>
2018-04-11 09:30:05 +0800

    解决了pyinstaller "main script return -1"问题

--- pyinstaller.py
+++ pyinstaller.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     from PyInstaller.__main__ import run
-    opts=['main.py','-D','-w','--icon=./asset/myicon.ico']
+    opts=['main.py','-F','-w','--hidden-import=queue','--icon=./asset/myicon.ico']
     run(opts)
